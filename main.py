039e71ae76f2a1505e7dbf79143d2df8c3ce28e2
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-23 13:49:01 -0400

    Update code to use external RTC
    - Code fetches time from an external RTC using I2C
    - Print out time on screen in 12 hour time w/ date
    
    - TODO: Print to display!

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from time import sleep
-
-from machine import Pin, Timer
-import utime
-
+from ds1307 import DS1307
+from machine import I2C, Pin, Timer
+from time import gmtime, time, sleep
 
 
 sleep(0.1) # Wait for USB to become ready
+
+month_name = ["", "January", "February", "March", "April",
+              "May", "June", "July","Augest", "September",
+              "October", "November", "December"]
 
 led = Pin(25, Pin.OUT)
 timer = Timer()
@@ -14,24 +16,40 @@ def blink(timer):
     led.toggle()
 
 timer.init(freq=1, mode=Timer.PERIODIC, callback=blink)
-print("Clock on!")
+print("Clock running!")
 
-# == Important code below ==
-sensor_temp = machine.ADC(machine.ADC.CORE_TEMP)
-conversion_factor = 3.3 / (65535)
+# == Important code below! ==
 
-rtc=machine.RTC()
-last_reading = None
+# As far as I understand the pi should get accurate
+# time from a time server, from which it will set it
+# but for in the simulator I'll comment it out
+# ds1307.datetime = gmtime(time())
 
-while True:
-    reading = sensor_temp.read_u16() * conversion_factor
-    timestamp=rtc.datetime()
-    temperature = 27 - (reading - 0.706)/0.001721
 
-    timestring="%04d-%02d-%02d %02d:%02d"%(timestamp[0:3] +timestamp[4:6])
+# Start reading from RTC clock
+i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=800000)
+ds1307 = DS1307(addr=0x68, i2c=i2c)
 
-    if last_reading != timestring:
-      print(timestring)
-      last_reading=timestring
+# convert 24-hr clock to 12-hr clock cause
+# i can't real 24-hr time and im not starting now!
+if ds1307.hour > 12:
+  hour = ds1307.hour - 12
+  period = "PM"
+else:
+  hour = ds1307.hour
+  period = "AM"
 
-    utime.sleep(1)
+# get the current RTC time
+# print("Current RTC time: {}".format(ds1307.datetime))
+
+print("Date: {} {:02d}, {:04d}".format(
+  month_name[ds1307.month],
+  ds1307.day,
+  ds1307.year
+))
+
+print("Time: {:02d}:{:02d} {}".format(
+  hour,
+  ds1307.minute,
+  period
+))
