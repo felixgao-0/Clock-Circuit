921dd6711f1869b2d01f4a7c27831cad788991ea
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-23 12:15:08 -0400

    Update main.py
    - Removed temp, decided I'd rather get the weather from the internet lol
    - We have the time!
    - TODO: ^ Use an RTC module for increased reliability

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import time
+from time import sleep
 
-import dht
-from machine import Pin, Timer, ADC
+from machine import Pin, Timer
+import utime
 
 
-time.sleep(0.1) # Wait for USB to become ready
+
+sleep(0.1) # Wait for USB to become ready
 
 led = Pin(25, Pin.OUT)
 timer = Timer()
@@ -15,11 +16,22 @@ def blink(timer):
 timer.init(freq=1, mode=Timer.PERIODIC, callback=blink)
 print("Clock on!")
 
+# == Important code below ==
+sensor_temp = machine.ADC(machine.ADC.CORE_TEMP)
+conversion_factor = 3.3 / (65535)
 
-#Pin modes here
-temp = dht.DHT22(machine.Pin(18))
-
+rtc=machine.RTC()
+last_reading = None
 
 while True:
-  print(temp.temperature())    
-  time.sleep(0.1)
+    reading = sensor_temp.read_u16() * conversion_factor
+    timestamp=rtc.datetime()
+    temperature = 27 - (reading - 0.706)/0.001721
+
+    timestring="%04d-%02d-%02d %02d:%02d"%(timestamp[0:3] +timestamp[4:6])
+
+    if last_reading != timestring:
+      print(timestring)
+      last_reading=timestring
+
+    utime.sleep(1)
