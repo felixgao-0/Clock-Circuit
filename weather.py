9abed2150ee1737a94f972a6e59a07926c8c058f
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-24 14:21:40 -0400

    Does asyncio improve performance?

--- weather.py
+++ weather.py
@@ -3,18 +3,17 @@
 # for the help w/ setting up the wifi simulator!
 
 import rp2
+import uasyncio as asyncio
 import network
 import ubinascii
 import machine
 import urequests as requests
 import time
 
+from timing import timeit
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-
-mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
-print('mac = {}'.format(mac))
 
 weather_str = None  # <= TODO: Weather info goes there
 
@@ -24,40 +23,45 @@ pw = ''
 # End network password stuff
 
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=43&longitude=-79&current=temperature_2m,relative_humidity_2m,weather_code&timezone=America%2FNew_York"
-
-def wifi_connected():
-    wlan_status = wlan.status()
-    if wlan_status != 3:
-        return False
-    else:
-        return True
-
-attempt_count = 0
-def connect_wifi():
-  while True:
-    if wifi_connected():
-      status = wlan.ifconfig()
-      print('ip = ' + status[0])
-      break
-    else:
-      print('Attempting to connect. Attempt #{}.'.format(attempt_count))
-      attempt_count += 1
-      led.off()
-      wlan.connect(ssid, pw)
-      time.sleep(3)
+url = "https://api.open-meteo.com/v1/forecast?latitude=43.7001&longitude=-79.4163&current=precipitation,weather_code&timezone=America%2FNew_York&forecast_days=1"
 
 
-connect_wifi()
+@timeit
+async def connect_wifi():
+  attempt_count = 0
+  while wlan.status() != 3: # Check if connected
+    attempt_count += 1
+    print('Connecting to {}. Attempt #{}.'.format(ssid, attempt_count))
+    wlan.connect(ssid, pw)
+    time.sleep(3)
+
+  status = wlan.ifconfig()
+  print('ip = {}'.format(status[0]))
 
 
-def fetch_weather():
-  if not wifi_connected():
-    connect_wifi()
-
+# In a real raspberry Pi apperently you can set a cron job which
+# would update the weather readings by calling this func every
+# few minutes. But this is a sim so im not gonna update to 
+# improve performance lolol. this sim is sloooooow!
+@timeit
+async def fetch_weather():
+  if wlan.status() != 3:
+    asyncio.run(connect_wifi())
+   
+  print('Starting request...')
   response = requests.get(url)
-  print(response)
-  response.close()
+  if response.status_code == 200:
+    weather_str = response.json()
+    response.close()
+  else:
+    print("aw no HTTP 200 - {}".format(response.text))
 
+
+# get the weather without making an api request
+# EVERY. SINGLE. TIME!!!
+@timeit
 def get_weather():
+  if not weather_str:
+    asyncio.run(fetch_weather())
+
   return weather_str
