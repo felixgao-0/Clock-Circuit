c335adf530e4e6198feb2df48308247dbc35d990
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-23 22:54:52 -0400

    WIP: Figure out POST requests

--- weather.py
+++ weather.py
@@ -1,6 +1,6 @@
 # A custom written api to fetch the weather easily
 
-import requests
+#import urequests
 
 
 url = "https://api.open-meteo.com/v1/forecast"
@@ -12,10 +12,27 @@ params = {
 	"forecast_days": 1
 }
 
+
+def http_get(url):
+    import socket
+    _, _, host, path = url.split('/', 3)
+    addr = socket.getaddrinfo(host, 80)[0][-1]
+    s = socket.socket()
+    s.connect(addr)
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+    s.close()
+
+
 weather_str = None  # <= TODO: Weather info goes there
 
 def get_weather():
   return weather_str
 
 def fetch_weather():
-  x = requests.post(url, json = myobj)
+  http_get(url)
