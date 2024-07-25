0f1e8278a373a3b56748bc2df60d4827151053d5
Felix G <76596162+API-Troubles@users.noreply.github.com>
2024-07-24 20:59:51 -0400

    Account for midnight time
    - Update time func to account for midnight conversion
    - Streamline functions with f-strings!

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from machine import I2C, Pin, Timer
 from time import gmtime, time, sleep
 
+from timing import timeit
+
 # Drivers for the RTC and the display!
+# Credit: https://github.com/mcauser/micropython-tinyrtc-i2c
 from ds1307 import DS1307
+
+# Credit: https://github.com/T-622/RPI-PICO-I2C-LCD
 from lcd_api import LcdApi
 from pico_i2c_lcd import I2cLcd
 
@@ -36,50 +41,48 @@ lcd.custom_char(1, bytearray([0x0E,0x0A,0x0E,0x00,
 
 # get time! THE TIME!
 # formatted as str so I can directly display
+# convert 24hr time => 12 hr time
 def get_time():
-  # convert 24hr time => 12 hr time
-  if ds1307.hour > 12:
-    hour = ds1307.hour - 12
-    period = "PM"
-  else:
     hour = ds1307.hour
     period = "AM"
 
-  return "{:02d}:{:02d}:{:02d} {}".format(
-    hour,
-    ds1307.minute,
-    ds1307.second,
-    period
-  )
+    if hour > 12:
+        hour -= 12
+        period = "PM"
+    elif hour == 0:
+        hour = 12  # Account for midnight
+    elif hour == 12:
+        period = "PM"
 
-# get the date ofc
-# formatted as str so I can directly display
+    return f"{hour:02d}:{ds1307.minute:02d}:{ds1307.second:02d} {period}"
+
+
 def get_date():
-  return "{} {}, {}".format(
-    month_name[ds1307.month],
-    ds1307.day,
-    ds1307.year
-  )
+  return f"{month_name[ds1307.month]} {ds1307.day}, {ds1307.year}"
 
 while True:
   lcd.clear()
 
   for _ in range(50):
+    timer = time()
     lcd.move_to(0,0)
     lcd.putstr(get_date())
+    if (ds1307.hour == 0) and (ds1307.minute == 0): 
+      lcd.move_to(0,0)
+      lcd.putstr(get_date())
+
     lcd.move_to(0,1)
     lcd.putstr(get_time())
 
-    sleep(0.2)
+
+    sleep(max(0, 1 - (timer - time())))
+
 
   lcd.clear()
 
-  #weather_data = weather.get_weather()
-
   lcd.move_to(0,0)
-  lcd.putstr("32{}c  50%".format(chr(1)))
+  lcd.putstr(f"32{chr(1)}c  50%")
   lcd.move_to(0,1)
-  lcd.putstr("hi")
+  lcd.putstr("Partly Cloudly")
 
-  for _ in range(50):
-    sleep(0.2)
+  sleep(10)
