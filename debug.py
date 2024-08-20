6769fde562f0aca0a8a13d671d954bbae18c63cc
Felix G <76596162+felixgao-0@users.noreply.github.com>
2024-08-20 11:50:11 -0400

    Add additional functions to debugger

--- debug.py
+++ debug.py
@@ -2,17 +2,24 @@
 class LoggerConfigException(Exception):
     pass
 
-
 class base_logger:
-    def __init__(self, *, level=None):
-        if level not in [None, "DEBUG", "INFO"]:
+    def __init__(self, *, level="WARNING"):
+        if level not in [None, "DEBUG", "INFO", "WARNING", "ERROR"]:
             raise LoggerConfigException("Invalid Level")
         self.level = level
 
     def debug(self, message):
         if self.level in ["DEBUG"]:
-            print(f"DEBUG: {message}")
+            print(f"DEBUG:   {message}")
 
     def info(self, message):
         if self.level in ["DEBUG", "INFO"]:
-            print(f"INFO: {message}")
+            print(f"INFO:    {message}")
+
+    def warning(self, message):
+        if self.level in ["DEBUG", "INFO", "WARNING"]:
+            print(f"WARNING: {message}")
+
+    def error(self, message):
+        if self.level in ["DEBUG", "INFO", "WARNING", "ERROR"]:
+            print(f"ERROR:   {message}")
